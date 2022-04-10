667aae10e3e5d37e2f95552a50632491a54770a8
yeol <rokmc741st@naver.com>
2022-04-10 17:28:29 +0900

    ex0703

---
+++ 07/ex0703/src/App.vue
@@ -0,0 +1,61 @@
+<!--
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+-->
+
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Multi Page</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> | 
+        <router-link to="/main">Main</router-link> | 
+        <router-link to="/sub">Sub</router-link>
+      </nav>
+      <v-slide-x-transition mode="out-in">
+        <router-view />
+      </v-slide-x-transition>
+    </v-main>
+    <v-footer color="secondary" dark>
+      <div class="mx-auto">HMAPS.net</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
