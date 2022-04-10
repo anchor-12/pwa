bebb1a4b25b81bf93003af314675b3873ca66ef0
yeol <rokmc741st@naver.com>
2022-04-10 15:01:10 +0900

    vue-cli

---
+++ 05/ex0507/src/views/MainView.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div>
+        <h2>Main Page</h2>
+        <button @click="fnSubPage">Sub</button>
+    </div>
+</template>
+<script>
+export default ({
+    methods: {
+        fnSubPage() {
+            this.$router.push('/sub')
+        }
+    }
+})
+</script>
---
+++ 05/ex0507/src/views/SubView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Sub Page</h2>
+        <button @click="fnMainPage">Main</button>
+    </div>
+</template>
+<script>
+
+export default ({
+    methods: {
+        fnMainPage() {
+            this.$router.push('/main');
+        }
+    }
+})
+</script>
