3a36a572eac4de5d13de92c294d6fe4c98f81be6
yeol <rokmc741st@naver.com>
2022-04-13 23:27:29 +0900

    08

---
+++ 08/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid fill-height>
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <h1 class="title white--text">Hello!</h1>
+            <p class="caption">by VueJS</p>
+            <img src="./assets/hello-pwa.png" alt="">
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
+};
+</script>
